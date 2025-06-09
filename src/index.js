27f603bc7ec2395d4618b902c5b7e4167d21705a
nguyenthihien51035 <hien51035@gmail.com>
2025-06-09 11:58:42 +0700

    MVC: routes, app/controller

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const exphbs = require('express-handlebars');
 const app = express()
 const port = 3000
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 // middleware
@@ -24,22 +26,9 @@ app.engine('hbs', exphbs.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-})
-
-app.get('/news', (req, res) => {
-    res.render('news');
-})
-
-app.get('/search', (req, res) => {
-    res.render('search')
-})
-
-app.post('/search', (req, res) => {
-    console.log(req.body)
-    res.send('')
-})
+// cau hinh routes (khoi tao tuyen duong)
+// route init
+route(app)
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
